d1a8db1a7700661396c1d59a154b6464400520f8
fran <fran-valero@hotmail.com>
2024-04-02 21:59:27 -0300

    styles

--- request.js
+++ request.js
@@ -1,17 +1,16 @@
-//realizamos la requeaste para llamara la api
+//realizamos la requeaste para llamara a la api
 
 const key = "d2569e92d2ed1899f24e3d6c3f89d14f";
 
+//llamada a la api del clima
 const requestCity = async city => {
     try {
-        const response = fetch(
+        const response = await fetch(
             `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&lang=es&appid=${key}`
         )
         const data = await response.json();
-            console.log(data);
             return data
     } catch (error) {
         console.log(error);
     }
-
-}
+};
